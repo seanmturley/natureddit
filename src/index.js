7f21d3cbf514016b888488571347d1ddc7efc485
Sean Turley <seanmturley@gmail.com>
2022-12-08 12:57:08 +0000

    [#9] Establish basic file structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root');
+import { Provider } from "react-redux";
+
+import { store } from "./app/store.js";
+import App from "./app/App.js";
+
+import "./index.css";
+
+import reportWebVitals from "./reportWebVitals.js";
+
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
