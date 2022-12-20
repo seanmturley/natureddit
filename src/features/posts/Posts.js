74b76b578f1c51990dc523695e095546ed584ff8
Sean Turley <seanmturley@gmail.com>
2022-12-20 21:51:16 +0000

    [#26] Add basic styling to Posts (using Magic Grid)

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Card from "../../components/card/Card";
 
 import { useSelector } from "react-redux";
 import { selectPostIds } from "./postsSlice";
 
+import "./Posts.css";
+
+import MagicGrid from "magic-grid";
+
 function Posts() {
   const postIds = useSelector(selectPostIds);
+
+  useEffect(() => {
+    let magicGrid = new MagicGrid({
+      container: ".posts__list",
+      items: postIds.length,
+      gutter: 0,
+      // useMin: true,
+      animate: true
+    });
+
+    magicGrid.listen();
+  });
 
   return (
     <section className="posts">
