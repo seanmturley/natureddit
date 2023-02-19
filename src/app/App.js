0122615fe867078fe2e94580108b464662dc943c
Sean Turley <seanmturley@gmail.com>
2023-02-19 19:21:20 +0000

    [#10] Set up basic routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-import { Routes, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+  Outlet
+} from "react-router-dom";
 
 import NavBar from "../features/navBar/NavBar";
 import Filters from "../features/filters/Filters";
@@ -8,23 +14,38 @@ import Posts from "../features/posts/Posts";
 
 import "./App.css";
 
+const NavBarLayout = () => (
+  <>
+    <NavBar />
+    <main className="app__main">
+      <Outlet />
+    </main>
+  </>
+);
+
+const FiltersLayout = () => (
+  <>
+    <Filters />
+    <Outlet />
+  </>
+);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<NavBarLayout />}>
+      <Route index element={<Posts />} />
+      <Route element={<FiltersLayout />}>
+        <Route path=":subreddit" element={<Posts />} />
+        <Route path="search" element={<Posts />} />
+      </Route>
+    </Route>
+  )
+);
+
 function App() {
   return (
     <div className="app">
-      <NavBar />
-      <main className="app__main">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Filters />
-                <Posts />
-              </>
-            }
-          />
-        </Routes>
-      </main>
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
-
 import { Provider } from "react-redux";
 
 import store from "./app/store.js";
@@ -19,9 +17,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 );
