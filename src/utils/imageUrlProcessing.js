bca8052420fabeac8df4e3de6d62ff3c6a408bc8
Sean Turley <seanmturley@gmail.com>
2023-03-01 20:24:26 +0000

    [#37] Increase specificity of guard clause to avoid attempts to access image data of posts where none exists

--- src/utils/imageUrlProcessing.js
+++ src/utils/imageUrlProcessing.js
@@ -1,6 +1,6 @@
 const getImageUrl = (post) => {
   // If no image
-  if (!post.preview) return null;
+  if (post.post_hint !== "image") return null;
 
   // This is currently hardcoded to arbitrarily choose the image resolution with width of 640px
   const encodedUrl = post.preview.images[0].resolutions[3].url;
