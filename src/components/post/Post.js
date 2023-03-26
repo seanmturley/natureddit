593953b655961ac264e2282070e52f2e475fd067
Sean Turley <seanmturley@gmail.com>
2023-03-26 23:55:19 +0100

    [#80] Add Post component HTML and data

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -4,14 +4,49 @@ import { useLoaderData } from "react-router-dom";
 
 import { useGetPostQuery } from "../../services/redditApi";
 
+import formatAge from "../../utils/ageFormatting";
+import formatNumber from "../../utils/numberFormatting";
+import getImageUrl from "../../utils/imageUrlProcessing";
+
 function Post() {
   const query = useLoaderData();
 
   const { data } = useGetPostQuery(query);
+  const post = data.post;
 
-  console.log(data);
+  const age = formatAge(post.created);
+  const formattedNumComments = formatNumber(post.num_comments);
+  const formattedScore = formatNumber(post.score);
+  const imageUrl = getImageUrl(post);
 
-  return <h1>Post</h1>;
+  return (
+    <div className="post-container">
+      <div className="post">
+        <img
+          className="post__image"
+          src={imageUrl}
+          alt={`${post.subreddit_name_prefixed} - ${post.title}`}
+        />
+        <section>
+          <div className="post__details">
+            Posted in{" "}
+            <span className="post__subreddit">
+              {post.subreddit_name_prefixed}
+            </span>{" "}
+            by <span>{post.author}</span>{" "}
+            <span className="post__age">{age}</span> ago
+          </div>
+          <h1 className="post__title">{post.title}</h1>
+          <div className="post__stats">
+            <div className="post__comments">{`${formattedNumComments} comments`}</div>
+            <div className="post__upvotes">{`${formattedScore} upvotes`}</div>
+            <div className="post__share">share</div>
+          </div>
+        </section>
+      </div>
+      {/* Render Comments */}
+    </div>
+  );
 }
 
 export default Post;
