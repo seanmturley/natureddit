5dede6b1f8b694843e1a764bb8e2942bace308e2
Sean Turley <seanmturley@gmail.com>
2023-06-03 18:00:42 +0100

    [#121] Add static search dropdown results and map over them

--- src/components/searchDropdown/SearchDropdown.js
+++ src/components/searchDropdown/SearchDropdown.js
@@ -2,30 +2,33 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+import "./SearchDropdown.css";
+
+const results = [
+  { name: "r/PATH", members: "124" },
+  { name: "r/pathofexile", members: "533k" },
+  { name: "r/Pathfinder_RPG", members: "141k" }
+];
+
 function SearchDropdown() {
   return (
     <div className="search-dropdown">
       <section className="search-dropdown__subreddits">
         <h1 className="search-dropdown__subreddits-heading">Subreddits</h1>
         <ul className="search-dropdown__subreddits-list">
-          <li className="subreddit">
-            <Link className="subreddit__link">
-              <div className="subreddit__icon"></div>
-              <div className="subreddit__details">
-                <div className="subreddit__name">r/PATH</div>
-                <div className="subreddit__members">124 members</div>
-              </div>
-            </Link>
-          </li>
-          <li className="subreddit">
-            <Link className="subreddit__link">
-              <div className="subreddit__icon"></div>
-              <div className="subreddit__details">
-                <div className="subreddit__name">r/pathofexile</div>
-                <div className="subreddit__members">533k members</div>
-              </div>
-            </Link>
-          </li>
+          {results.map((result, index) => (
+            <li className="subreddit" key={index}>
+              <Link className="subreddit__link" to={`../${result.name}/hot`}>
+                <div className="subreddit__icon"></div>
+                <div className="subreddit__details">
+                  <div className="subreddit__name">{result.name}</div>
+                  <div className="subreddit__members">
+                    {result.members} members
+                  </div>
+                </div>
+              </Link>
+            </li>
+          ))}
         </ul>
       </section>
       <section className="search-dropdown__search">
