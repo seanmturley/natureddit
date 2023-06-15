8a88b759430d373ef2be395113f9be4d38b257ef
Sean Turley <seanmturley@gmail.com>
2023-06-15 12:35:51 +0100

    [#129] Expand formatNumber function do deal with numbers in the millions

--- src/utils/numberFormatting.js
+++ src/utils/numberFormatting.js
@@ -3,12 +3,22 @@ const formatNumber = (number) => {
     return number;
   }
 
-  const convertedNumber = number / 1000; // For display with "k" suffix
+  if (number < 1e6) {
+    const numberThousands = number / 1000; // For display with "k" suffix
 
-  if (number < 100000) {
-    return `${Math.round(convertedNumber * 10) / 10}k`; // Multiplication and division to get 1dp
+    if (number < 1e5) {
+      return `${Math.round(numberThousands * 10) / 10}k`; // Multiplication and division to get 1dp
+    } else {
+      return `${Math.round(numberThousands)}k`;
+    }
+  }
+
+  const numberMillions = number / 1e6;
+
+  if (number < 1e8) {
+    return `${Math.round(numberMillions * 10) / 10}m`; // Multiplication and division to get 1dp
   } else {
-    return `${Math.round(convertedNumber)}k`;
+    return `${Math.round(numberMillions)}m`;
   }
 };
 
