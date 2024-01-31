e92cb99ba66b0dbb8af8a04ab3084f980e37b669
Sean Turley <seanmturley@gmail.com>
2024-01-31 22:33:39 +1300

    [#161] Add utility function for decoding HTML entities

---
+++ src/utils/htmlDecoding.js
@@ -0,0 +1,10 @@
+// This is the recommended way to decode HTML entities in a manner
+// that's robust to XSS attacks.
+// Taken from: https://stackoverflow.com/questions/1912501/unescape-html-entities-in-javascript/34064434#34064434
+
+const htmlDecode = (encodedString) => {
+  const doc = new DOMParser().parseFromString(encodedString, "text/html");
+  return doc.documentElement.textContent;
+};
+
+export default htmlDecode;
