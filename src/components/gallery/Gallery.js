23e1006027da23922c4b4046edf6a69019271525
Sean Turley <seanmturley@gmail.com>
2024-02-14 23:48:39 +1300

    [#162] Add basic Gallery component

---
+++ src/components/gallery/Gallery.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+function Gallery({ images }) {
+  return (
+    <ul>
+      {images.map((image) => (
+        <li>
+          <img
+            className="gallery__image"
+            src={image.medium}
+            alt={`Alt text with number in gallery e.g. "2 of 4"`}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+Gallery.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+export default Gallery;
