cea71a97aa6ac85ff1e89165dd0686f29fa870a3
Sean Turley <seanmturley@gmail.com>
2024-03-22 17:20:54 +1300

    [#160] Add additional subreddits from r/sfwpornnetwork to the homepage

--- src/services/apiLoaders.js
+++ src/services/apiLoaders.js
@@ -51,9 +51,13 @@ export const cardsLoader = async ({ request }) => {
       paths: [
         { subreddit: "r/AutumnPorn/hot", limit: "10" },
         { subreddit: "r/beautifultrees/hot", limit: "10" },
+        { subreddit: "r/desertporn/hot", limit: "10" },
         { subreddit: "r/EarthPorn/hot", limit: "10" },
+        { subreddit: "r/lakeporn/hot", limit: "10" },
         { subreddit: "r/LandscapePhotography/hot", limit: "10" },
         { subreddit: "r/natureporn/hot", limit: "10" },
+        { subreddit: "r/seaporn/hot", limit: "10" },
+        { subreddit: "r/waterporn/hot", limit: "10" },
         { subreddit: "r/winterporn/hot", limit: "10" }
       ],
       numPostsRequired: 40
