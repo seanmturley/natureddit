5a4b9cad38a0393d8301def69171a47d42d181e8
Sean Turley <seanmturley@gmail.com>
2024-03-31 19:20:40 +1300

    [#168] Revert 3 previous commits that attempted a loading page on initial load with a useEffect in App

--- src/app/App.js
+++ src/app/App.js
@@ -1,35 +1,81 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements
+} from "react-router-dom";
 
 import useLocalStorage from "use-local-storage";
 
+import MainLayout from "../layouts/mainLayout/MainLayout";
+import FiltersLayout from "../layouts/filtersLayout/FiltersLayout";
+import ErrorPage from "../components/errorPage/ErrorPage";
+import CardsContainer from "../components/cardsContainer/CardsContainer";
+import Post from "../components/post/Post";
+
+import { cardsLoader, postLoader } from "../services/apiLoaders";
+
+import basename from "../utils/baseName";
+
 import "./App.css";
 
-function App({ router }) {
+const modalRoute = (
+  <Route
+    path="modal/r/:subreddit/comments/:id/:title/:commentId?"
+    loader={postLoader}
+    element={<Post modal={true} />}
+  />
+);
+
+const router = (lightTheme, setLightTheme) =>
+  createBrowserRouter(
+    createRoutesFromElements(
+      <Route
+        element={
+          <MainLayout lightTheme={lightTheme} setLightTheme={setLightTheme} />
+        }
+      >
+        <Route errorElement={<ErrorPage />}>
+          <Route path="/" loader={cardsLoader} element={<CardsContainer />}>
+            {modalRoute}
+          </Route>
+          <Route element={<FiltersLayout />}>
+            <Route
+              path="/r/:subreddit/:sortFilter"
+              loader={cardsLoader}
+              element={<CardsContainer />}
+            >
+              {modalRoute}
+            </Route>
+            <Route
+              path="/search"
+              element={<CardsContainer />}
+              loader={cardsLoader}
+            >
+              {modalRoute}
+            </Route>
+          </Route>
+          <Route
+            path="/fullpage/r/:subreddit/comments/:id/:title/:commentId?"
+            loader={postLoader}
+            element={<Post modal={false} />}
+          />
+        </Route>
+      </Route>
+    ),
+    {
+      basename: `${basename}/`
+    }
+  );
+
+function App() {
   const [lightTheme, setLightTheme] = useLocalStorage("lightTheme", false);
-  const [showSplashScreen, setShowSplashScreen] = useState(true);
-
-  useEffect(() => {
-    const splashScreenInterval = setInterval(() => {
-      const navState = router().state.navigation.state;
-
-      if (navState === "idle") {
-        setShowSplashScreen(false);
-        clearInterval(splashScreenInterval);
-      }
-    }, 1000);
-
-    return () => clearInterval(splashScreenInterval);
-  }, [router]);
 
   return (
     <div className="app" data-light-theme={lightTheme}>
-      {showSplashScreen ? (
-        <h1>Loading...</h1>
-      ) : (
-        <RouterProvider router={router(lightTheme, setLightTheme)} />
-      )}
+      <RouterProvider router={router(lightTheme, setLightTheme)} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import {
-  createBrowserRouter,
-  Route,
-  createRoutesFromElements
-} from "react-router-dom";
-
 import { Provider } from "react-redux";
 
 import store from "./store.js";
 import App from "./app/App.js";
-
-import MainLayout from "./layouts/mainLayout/MainLayout";
-import FiltersLayout from "./layouts/filtersLayout/FiltersLayout";
-import ErrorPage from "./components/errorPage/ErrorPage";
-import CardsContainer from "./components/cardsContainer/CardsContainer";
-import Post from "./components/post/Post";
-
-import { cardsLoader, postLoader } from "./services/apiLoaders";
-
-import basename from "./utils/baseName";
 
 import "normalize.css";
 import "./index.css";
@@ -30,59 +14,10 @@ import reportWebVitals from "./reportWebVitals.js";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-const modalRoute = (
-  <Route
-    path="modal/r/:subreddit/comments/:id/:title/:commentId?"
-    loader={postLoader}
-    element={<Post modal={true} />}
-  />
-);
-
-const router = (lightTheme, setLightTheme) =>
-  createBrowserRouter(
-    createRoutesFromElements(
-      <Route
-        element={
-          <MainLayout lightTheme={lightTheme} setLightTheme={setLightTheme} />
-        }
-      >
-        <Route errorElement={<ErrorPage />}>
-          <Route path="/" loader={cardsLoader} element={<CardsContainer />}>
-            {modalRoute}
-          </Route>
-          <Route element={<FiltersLayout />}>
-            <Route
-              path="/r/:subreddit/:sortFilter"
-              loader={cardsLoader}
-              element={<CardsContainer />}
-            >
-              {modalRoute}
-            </Route>
-            <Route
-              path="/search"
-              element={<CardsContainer />}
-              loader={cardsLoader}
-            >
-              {modalRoute}
-            </Route>
-          </Route>
-          <Route
-            path="/fullpage/r/:subreddit/comments/:id/:title/:commentId?"
-            loader={postLoader}
-            element={<Post modal={false} />}
-          />
-        </Route>
-      </Route>
-    ),
-    {
-      basename: `${basename}/`
-    }
-  );
-
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
